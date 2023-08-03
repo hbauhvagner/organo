5bb9bda8d97c9d02515093ac13a2d84a44f1232a
hbauhvagner <hbauhvagner@gmail.com>
2023-08-02 23:59:30 -0300

    Manipulando o input do usuário

--- src/components/TextField/index.js
+++ src/components/TextField/index.js
@@ -1,10 +1,17 @@
 import './style.css'
 
 const TextField = (props) => {
+    let inputValue = ''
+
+    const enteredValue = (e) => {
+        inputValue = e.target.value
+        console.log(inputValue)
+    }
+
     return (
         <div className="text-field">
             <label>{props.label}</label>
-            <input required={props.required} placeholder={props.placeholder} />
+            <input value={inputValue} onChange={enteredValue} required={props.required} placeholder={props.placeholder} />
         </div>
     )
 }
