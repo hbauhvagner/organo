1a0e1aad1f6c69f3902cb6d64b0979b9958eee19
hbauhvagner <hbauhvagner@gmail.com>
2023-08-02 23:19:28 -0300

    Criando o botão

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,3 +1,4 @@
+import Button from '../Button'
 import DropdownList from '../DropdownList'
 import TextField from '../TextField'
 import './style.css'
@@ -22,6 +23,9 @@ const Form = () => {
                 <TextField label="Cargo" placeholder="Digite seu cargo" />
                 <TextField label="Imagem" placeholder="Digite o endereço da imagem" />
                 <DropdownList label='Time' itens={times} />
+                <Button>
+                    Criar Card
+                </Button>
             </form>
         </section>
     )
