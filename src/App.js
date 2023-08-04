f6cf921e871b153ead081eebe299506e463d28d7
hbauhvagner <hbauhvagner@gmail.com>
2023-08-04 00:21:08 -0300

    Criando o componente Time

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './components/Banner';
 import Form from './components/Form';
+import Team from './components/Team';
 
 function App() {
   const [users, setUsers] = useState([])
@@ -14,6 +15,9 @@ function App() {
     <div className="App">
       <Banner />
       <Form registeredUser={user => newUserAdded(user)} />
+      <Team name='Programação' />
+      <Team name='Front-end' />
+      <Team name='Data Science' />
     </div>
   );
 }
