a47cb02cfbc85361de940cdc8d0c8a68740d91be
hbauhvagner <hbauhvagner@gmail.com>
2023-08-09 00:15:34 -0300

    Elevando a lista de times

--- src/App.js
+++ src/App.js
@@ -4,6 +4,44 @@ import Form from './components/Form';
 import Team from './components/Team';
 
 function App() {
+  const teams = [
+    {
+      name: 'Programação',
+      primaryColor: '#57c278',
+      secondaryColor: '#d9f7e9'
+    },
+    {
+      name: 'Front-End',
+      primaryColor: '#82CFFA',
+      secondaryColor: '#E8F8FF'
+    },
+    {
+      name: 'Data Science',
+      primaryColor: '#A6D157',
+      secondaryColor: '#F0F8E2'
+    },
+    {
+      name: 'Devops',
+      primaryColor: '#E06B69',
+      secondaryColor: '#FDE7E8'
+    },
+    {
+      name: 'UX e Design',
+      primaryColor: '#DB6EBF',
+      secondaryColor: '#FAE9F5'
+    },
+    {
+      name: 'Mobile',
+      primaryColor: '#FFBA05',
+      secondaryColor: '#FFF5D9'
+    },
+    {
+      name: 'Inovação e Gestão',
+      primaryColor: '#FF8A29',
+      secondaryColor: '#FFEEDF'
+    },
+  ]
+
   const [users, setUsers] = useState([])
 
   const newUserAdded = (user) => {
@@ -15,9 +53,7 @@ function App() {
     <div className="App">
       <Banner />
       <Form registeredUser={user => newUserAdded(user)} />
-      <Team name='Programação' />
-      <Team name='Front-end' />
-      <Team name='Data Science' />
+      {teams.map(team => <Team key={team.name} name={team.name} primaryColor={team.primaryColor} secondaryColor={team.secondaryColor} />)}
     </div>
   );
 }
