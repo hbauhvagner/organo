820ffe768564bfae965b28f804dde3ec8930fe9c
hbauhvagner <hbauhvagner@gmail.com>
2023-08-09 22:08:59 -0300

    Card de colaborador

---
+++ src/components/Collaborator/index.js
@@ -0,0 +1,17 @@
+import './style.css'
+
+const Collaborator = () => {
+    return (
+        <div className='collaborator'>
+            <div className='header'>
+                <img src="https://github.com/hbauhvagner.png" alt="Vagner Nascimento" />
+            </div>
+            <div className='baseboard'>
+                <h4>Vagner Nascimento</h4>
+                <h5>Dev</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Collaborator
--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,9 +1,12 @@
+import Collaborator from '../Collaborator'
 import './style.css'
 
 const Team = (props) => {
     return (
         <section className='team' style={{ backgroundColor: props.secondaryColor }}>
             <h3 style={{ borderColor: props.primaryColor }}>{props.name}</h3>
+
+            <Collaborator />
         </section>
     )
 }
